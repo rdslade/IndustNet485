5b4b0da67905b86a3aa11717243fabe31f882f62
rdslade <rdslade@umich.edu>
2018-05-02 15:53:18 -0400

    temp fix on populating group page, loading all group select options, and changing all jquery to native JS

--- js/support.js
+++ js/support.js
@@ -43,7 +43,19 @@ function procMapDataReply(replyText){
         setFormField(fld,vpair[1]);
   }
 }
+function loadJSON(file, callback) {   
 
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
 function mapFldToVar(fname)
 {
   for (var i = 0; i < fldVarPair.length; i++)
